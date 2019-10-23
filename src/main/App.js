1244c8ffcd671e38dea5da353785c7a4cb020f48
Monique Kurkowski <monique@momo.fritz.box>
2019-10-23 21:30:16 +0200

    Zweite Änderung

--- src/main/App.js
+++ src/main/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+//Test
 import Alert from 'react-bootstrap-sweetalert';
 import "../css/App.css"
 import * as dgapi from '../utils/API/dgapi'
@@ -6,89 +7,99 @@ import LoadingScreen from "./body/components/LoadingScreen"
 import Body from "./body/index"
 import devData from "../utils/API/devData"
 class App extends Component {
-  constructor(props) {
-      super(props) 
-      this.state = {
-        devMode: true,    //Entwickler Modus -> Entwickler Daten
-        loading: true,    //Loadingscreen anzeigen?
-        notify:{          //Object für die Benachrichtigung
-            title: "",
-            message: "",
-            status: false,
-            type: "default"
-        },
-        data: {
-          systeme: {},
-          kunden: {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            devMode: true, //Entwickler Modus -> Entwickler Daten
+            loading: true, //Loadingscreen anzeigen?
+            notify: { //Object für die Benachrichtigung
+                title: "",
+                message: "",
+                status: false,
+                type: "default"
+            },
+            data: {
+                systeme: {},
+                kunden: {}
+            }
         }
-      }
-      this._handleChange = this._handleChange.bind(this)
-      this._handleSubmit = this._handleSubmit.bind(this)
-  }
-  async componentDidMount(){
-    var kunden = {}
-    var systeme = {}
-    if(this.state.devMode === true){
-      this.setState({
-        loading: false,
-        data: {
-          systeme: devData.systeme,
-          kunden: devData.kunden
-        }
-      })
-    }else{
-      await dgapi.getAllKunden().then(async (responseKunden)=>{
-        kunden = responseKunden
-        }).then(async ()=>{
-          await dgapi.getAllSystems().then((responseSysteme)=>{
-          systeme = responseSysteme
-            this.setState({ 
-              data: {
-                systeme: systeme,
-                kunden: kunden
-              },         
-              loading: false
+        this._handleChange = this._handleChange.bind(this)
+        this._handleSubmit = this._handleSubmit.bind(this)
+    }
+    async componentDidMount() {
+        var kunden = {}
+        var systeme = {}
+        if (this.state.devMode === true) {
+            this.setState({
+                loading: false,
+                data: {
+                    systeme: devData.systeme,
+                    kunden: devData.kunden
+                }
             })
-        }) 
-      })
-    }  
-  }
-  _handleChange(event){
-      var change = {}
-      Object.assign(change,{[event.id]: { value: event.value}})
-      this.setState({
-        change: change
-      })
-  }
-  _handleSubmit(event){
-    console.log("Submit Detected: ", event)
-    event.preventDefault();
-    this.setState({
-      notify:{
-        status: true
-      }
-    })
-  }
-  render() {
-    console.log("App-State", this.state)
-    if(this.state.loading === true) return <LoadingScreen type="balls" color="#A61609" className="LoadingScreen" />
-    return (
-        <div>
-            <Alert
-                title={this.state.notify.title}
-                show={this.state.notify.status === true}
-                text={this.state.notify.message}
-                type={this.state.notify.type}
-                onConfirm={() => this.setState({notify: {
-                  status: false,
-                  title: this.state.notify.title,
-                }})}>{this.state.notify.message}
-            </Alert>
-          <Body App={this.state} handleChange={this._handleChange} handleSubmit={this._handleSubmit}/>
-        </div>
-      )
+        } else {
+            await dgapi.getAllKunden().then(async(responseKunden) => {
+                kunden = responseKunden
+            }).then(async() => {
+                await dgapi.getAllSystems().then((responseSysteme) => {
+                    systeme = responseSysteme
+                    this.setState({
+                        data: {
+                            systeme: systeme,
+                            kunden: kunden
+                        },
+                        loading: false
+                    })
+                })
+            })
+        }
+    }
+    _handleChange(event) {
+        var change = {}
+        Object.assign(change, {
+            [event.id]: { value: event.value } })
+        this.setState({
+            change: change
+        })
+    }
+    _handleSubmit(event) {
+        console.log("Submit Detected: ", event)
+        event.preventDefault();
+        this.setState({
+            notify: {
+                status: true
+            }
+        })
+    }
+    render() {
+        console.log("App-State", this.state)
+        if (this.state.loading === true) return <LoadingScreen type = "balls"
+        color = "#A61609"
+        className = "LoadingScreen" / >
+            return ( <
+                div >
+                <
+                Alert title = { this.state.notify.title }
+                show = { this.state.notify.status === true }
+                text = { this.state.notify.message }
+                type = { this.state.notify.type }
+                onConfirm = {
+                    () => this.setState({
+                        notify: {
+                            status: false,
+                            title: this.state.notify.title,
+                        }
+                    })
+                } > { this.state.notify.message } <
+                /Alert> <
+                Body App = { this.state }
+                handleChange = { this._handleChange }
+                handleSubmit = { this._handleSubmit }
+                /> <
+                /div>
+            )
 
-  }
+    }
 }
 
 export default App;
