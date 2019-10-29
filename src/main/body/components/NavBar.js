48be1a616a800545516d7c05038d147e19cf1270
Stephan Gärtner <stephan.gaertner72@outlook.de>
2019-10-29 12:50:50 +0100

    add dropdown to navbar

--- src/main/body/components/NavBar.js
+++ src/main/body/components/NavBar.js
@@ -1,5 +1,6 @@
 import React ,{Component} from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, NavDropdown } from 'react-bootstrap';
 
 
 
@@ -14,19 +15,26 @@ class Navigation extends Component {
     render() {
         return (
             <div className="navbar">
-                <Link to="/">
-                    <p>Dashboard</p>
-                </Link>
-                <Link to="/system/new">
-                    <p>Neues System</p>
-                </Link>
-                <Link to="/system/edit">
-                    <p>Edit System</p>
-                </Link>
-                <Link to="/system/upload">
-                    <p>Upload</p>
-                </Link>
+                <Navbar bg="light" expand="lg">
+                    <Link to="/">
+                        <p>Dashboard</p>
+                    </Link>
+                    <NavDropdown eventKey={3} title="Systeme" id="basic-nav-    dropdown">
+                        <NavDropdown.Item href="/system/new" className="NavbarItem">New System</NavDropdown.Item>
+                        <NavDropdown.Item href="/system/edit">Edit System</NavDropdown.Item>
+                    </NavDropdown>
+                    {/* <Link to="/system/new">
+                        <p>Neues System</p>
+                    </Link> */}
+                    {/* <Link to="/system/edit">
+                        <p>Edit System</p>
+                    </Link> */}
+                    <Link to="/system/upload">
+                        <p>Upload</p>
+                    </Link>
+                </Navbar>
             </div>
+
         )
     }
 }
